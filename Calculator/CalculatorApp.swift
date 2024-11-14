a14e0a7c748efe1449ced1527aa9603ac65ae79c
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-11-14 22:44:52 +0530

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Vaibhav Kesarwani on 14/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Vaibhav Kesarwani on 14/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
