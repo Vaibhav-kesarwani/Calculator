372e2fb817f8279e2be5e58cfc404b379046d723
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-11-14 23:55:08 +0530

    Added the buttons to the swift ui screen for the buttons to visible

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -49,7 +49,20 @@ struct ContentView: View {
                 
                 
                 // Our buttons
-                
+                ForEach(buttons, id: \.self) { row in
+                    ForEach(row, id: \.self) { item in
+                        Button(action: {
+                            
+                        }, label: {
+                            Text(item.rawValue)
+                                .frame(width: 70, height: 70)
+                                .background(Color.orange)
+                                .foregroundColor(.white)
+                                .foregroundColor(.white)
+                                .cornerRadius(35)
+                        })
+                    }
+                }
             }
         }
     }
